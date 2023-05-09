b225b890d7ca3aae4cc0ad30bb73d53134da318b
Aster <galaster@foxmail.com>
2023-05-10 03:13:52 +0800

    Parse for valid Symbol

--- projects/ruler-types/src/lib.rs
+++ projects/ruler-types/src/lib.rs
@@ -4,5 +4,6 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
-pub use graph_types::{GraphResult, GraphError, GraphErrorKind};
 mod graphs;
+mod variable;
+
