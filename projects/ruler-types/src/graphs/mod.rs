07f0edc421e792e191db92c10ca5f25262a3ed24
Aster <galaster@foxmail.com>
2023-05-10 02:49:00 +0800

    Define the EHead and ENode

--- projects/ruler-types/src/graphs/mod.rs
+++ projects/ruler-types/src/graphs/mod.rs
@@ -1,5 +1,42 @@
+use std::collections::BTreeMap;
 use adjacency_list::AdjacencyNodeList;
 
 pub struct EGraph {
-    engine: AdjacencyNodeList,
+    head_nodes: BTreeMap<usize, usize>,
+    last_edge: usize,
+    nodes: BTreeMap<usize, usize>,
+}
+
+pub enum Expr {
+    Add {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    Sub {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    Atom {
+        value: f32
+    },
+}
+
+pub struct Expression<H> {
+    head: EHead<H>,
+    rest: Vec<ENode<Self>>,
+}
+
+pub struct EHead<H> {
+    id: usize,
+    data: H,
+}
+
+pub struct ENode<N> {
+    id: usize,
+    data: N,
+}
+
+pub struct ENeighbors {
+    end_nodes: BTreeMap<u32, u32>,
+    links: BTreeMap<u32, u32>,
 }
